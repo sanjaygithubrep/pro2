213e068087d8ca4984feb122a9b2979c442652e5
Sanjay Singh Rao <sanjayraoce@gmail.com>
2022-12-06 23:02:33 +0530

    pro1

---
+++ script.js
@@ -0,0 +1,31 @@
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+const c = document.getElementById("c");
+const d = document.getElementById("d");
+const e = document.getElementById("e");
+const f = document.getElementById("f");
+const g = document.getElementById("g");
+const h = document.getElementById("h");
+const i = document.getElementById("i");
+const j = document.getElementById("j");
+const k = document.getElementById("k");
+const l = document.getElementById("l");
+const m = document.getElementById("m");
+const n = document.getElementById("n");
+const o = document.getElementById("o");
+const p = document.getElementById("p");
+const q = document.getElementById("q");
+const r = document.getElementById("r");
+const s = document.getElementById("s");
+const t = document.getElementById("t");
+const u = document.getElementById("u");
+const v = document.getElementById("v");
+const w = document.getElementById("w");
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+const z = document.getElementById("z");
+const num = document.getElementById("0-9");
+
+// a.addEventListener('click' ()=>{
+
+// })
